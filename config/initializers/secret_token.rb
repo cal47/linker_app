edde4a654ecd92775bf7e961744c88f325ba0509
Christopher Loncar <cloncar@gmail.com>
2013-10-07 15:47:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkerApp::Application.config.secret_token = '058be208bfef149f3530948a783917481f215a29867870adff0984d1a1ce50308b385e80116a6f272e8a61f8cfdd3871503a1c0abcb590572e5db8fbad3916f7'
